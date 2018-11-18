96636f5b2843753301b57d396a287cc942d87e50
LPF <1092156308@qq.com>
2018-11-18 10:53:57 +0800

    第一版,正在做第二页

---
+++ 5、源代码/meizu-webapp/src/components/flag/flagHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <div>
+          <a href="#"><i></i></a>
+        </div>
+
+        <div>
+            
+        </div>
+        <div></div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 5、源代码/meizu-webapp/src/pages/flag.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="all">
+        <header></header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "flag"
+    }
+</script>
+
+<style scoped>
+
+</style>
